2ad994db9627522af57d59261456943ea36caf0b
hassan yaqoob <hassan@aspnetzerodevelopers.com>
2023-04-09 20:06:38 +0500

    user location table add

---
+++ aspnet-core/src/AuctionSystem.Core/UserLocation/UserLocation.cs
@@ -0,0 +1,28 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using AuctionSystem.Authorization.Users;
+
+namespace AuctionSystem.UserLocation
+{
+    [Table("UserLocations")]
+    public class UserLocation : FullAuditedEntity<long>, IMustHaveTenant
+    {
+
+        public virtual long? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User UserIdFk { get; set; }
+        public virtual long? LocationId { get; set; }
+
+        [ForeignKey("LocationId")]
+        public AuctionSystem.Location.Location LocationIdFk { get; set; }
+        public int TenantId { get; set; }
+
+    }
+}
--- aspnet-core/src/AuctionSystem.EntityFrameworkCore/Migrations/20230409150525_userLocationTableAdd.Designer.cs
+++ aspnet-core/src/AuctionSystem.EntityFrameworkCore/Migrations/20230409150525_userLocationTableAdd.Designer.cs
@@ -4,6 +4,7 @@ using AuctionSystem.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuctionSystem.Migrations
 {
     [DbContext(typeof(AuctionSystemDbContext))]
-    partial class AuctionSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230409150525_userLocationTableAdd")]
+    partial class userLocationTableAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
